16a95e9bde4eb733e94bc3f49b57cf46ec31dd32
John <jonatascleytonf@gmail.com>
2024-11-08 18:58:48 -0300

    versão finalizada

--- desafio1/main.c
+++ desafio1/main.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+void imprimir_bin(unsigned char item){
+
+    puts("Armários:");
+    printf("8 7 6 5 4 3 2 1\n");
+
+    for(int i = 7; i >= 0; --i){
+        printf("%d ", (item >> i) & 1);
+    }
+    puts("\n\n\n");
+}
+
 unsigned char escolha(unsigned char armarios){
     unsigned char old = armarios;
     unsigned char pos;
@@ -9,22 +20,21 @@ unsigned char escolha(unsigned char armarios){
     srand(time(NULL));
     do{
         unsigned char mask = 0x01;
-       
+
         pos = 1 + rand() % 8;
 
         mask <<= (pos-1);
         armarios = armarios | mask;
 
     if (armarios >= 255) {
-        break;  
+        break;
     } else {
-        continue;  
+        continue;
 }
 
     }while(armarios == old);
 
-    printf("%hhu\n", armarios); // será substituido por uma lógica dde imprimir string binária
-
+    imprimir_bin(armarios);
     return armarios;
 }
 
@@ -34,12 +44,13 @@ unsigned char libera(unsigned char armarios, unsigned char pos){
 
     armarios = armarios & ~(mask << (pos - 1));
 
-    printf("%d \n", armarios);// será substituido por uma lógica dde imprimir string binária
-    return armarios; 
+    imprimir_bin(armarios);
+
+    return armarios;
 }
 
 
-int main(int argc, char *argv[]){
+int main(void){
     unsigned char armarios = 0x00;
     unsigned char input = 0;
     unsigned char pos;
@@ -64,10 +75,10 @@ int main(int argc, char *argv[]){
             case 3:
                 break;
             default:
-                puts("Opção inválida");
+                imprimir_bin(armarios);
+                break;
         }
     } while (input != 3);
 
   return 0;
 }
-
